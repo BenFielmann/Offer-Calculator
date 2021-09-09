aa4d7c844b50bd0686b87fab3881a80a1dd5f4a9
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-09 14:05:33 +0200

    🍱 Add all needed Icons

---
+++ src/app/components/assets/ForwardButtonIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function ForwardButtonIcon(
+  props: React.SVGProps<SVGSVGElement>
+): JSX.Element {
+  return (
+    <svg
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M24.1667 12.5L11.6667 25V19.1667H0V5.83333H11.6667V0L24.1667 12.5Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
