26010fc6575135641587b187c23b86048fa70bdb
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-16 16:18:17 +0200

    💄 Forgot to run prettier

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import './CustomReactCalendar.css'
+import './CustomReactCalendar.css';
 
 export default function ReactCalendar(): JSX.Element {
   const [value, onChange] = useState(new Date());
