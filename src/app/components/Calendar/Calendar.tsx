198f84975ef828f3e6c46884a099bec873331d36
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-16 15:27:56 +0200

    ✨ Create calendar component

---
+++ src/app/components/Calendar/Calendar.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './CustomReactCalendar.css'
+
+export default function ReactCalendar(): JSX.Element {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+}
