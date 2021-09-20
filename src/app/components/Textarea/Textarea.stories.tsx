5ecf9aaadf7b27e8111ab08bad63df0f03387e31
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-20 16:17:51 +0200

    ✨ Create two states of text area

---
+++ src/app/components/Textarea/Textarea.stories.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Textarea from './Textarea';
+
+export default {
+  title: 'Component/Textarea',
+  component: Textarea,
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+};
+
+export const EditableTextArea = (): JSX.Element => (
+  <Textarea
+    isEditable={true}
+    setIsEditable={console.log}
+    companyValue="Muster Firma"
+    setCompanyValue={console.log}
+    ceoValue=""
+    setCeoValue={console.log}
+    phoneNumberValue=""
+    setPhoneNumberValue={console.log}
+    mobileValue=""
+    setMobileValue={console.log}
+    emailValue=""
+    setEmailValue={console.log}
+    urlValue=""
+    setUrlValue={console.log}
+    streetValue=""
+    setStreetValue={console.log}
+    plzValue=""
+    setPlzValue={console.log}
+    cityValue=""
+    setCityValue={console.log}
+    managerValue=""
+    setManagerValue={console.log}
+    managerPhoneValue=""
+    setManagerPhoneValue={console.log}
+  />
+);
+export const TextArea = (): JSX.Element => (
+  <Textarea
+    isEditable={false}
+    setIsEditable={console.log}
+    companyValue="Muster Firma"
+    ceoValue="Muster Mann"
+    phoneNumberValue="0...."
+    mobileValue="0...."
+    emailValue="Muster@mail.de"
+    urlValue="www.Muster.de"
+    streetValue="Muster Straße"
+    plzValue="0...."
+    cityValue="Muster Stadt"
+    managerValue="Muster Angestellter"
+    managerPhoneValue="0...."
+  />
+);
