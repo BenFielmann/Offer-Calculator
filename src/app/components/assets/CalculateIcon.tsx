a1c734965f5413756075ea0310ce7a3db1d8feb2
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-22 17:02:28 +0200

    💄 Change color of CalculatorIcon

--- src/app/components/assets/CalculateIcon.tsx
+++ src/app/components/assets/CalculateIcon.tsx
@@ -8,7 +8,7 @@ export default function CalculateIcon(
       width="30"
       height="30"
       viewBox="0 0 30 30"
-      fill="black"
+      fill="#03BEF9"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
