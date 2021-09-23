512fbecb051384879c68170274de1a5261cbf1d3
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-23 18:31:08 +0200

    🎨 Add first route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import logo from './components/assets/Worker.png';
+import Home from './Pages/Home/Home';
+/*import ProfileOverview from './Pages/ProfileOverview/ProfileOverview';*/
+
 import styles from './App.module.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -7,20 +9,17 @@ function App(): JSX.Element {
   return (
     <BrowserRouter>
       <div className={styles.App}>
-        <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-          <Switch>
-            <Route path="/about">
-              <main>About</main>
-            </Route>
-            <Route path="/">
-              <main>Home</main>
-            </Route>
-          </Switch>
-        </header>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+/*<img src={logo} className={styles['App-logo']} alt="logo" />
+import logo from './components/assets/Worker.png';*/
