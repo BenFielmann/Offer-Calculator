30a6fd9435269eec93f1f517f64f20b5b10ab25d
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-23 23:19:55 +0200

    🔥 remove onClick function

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import BurgerIcon from '../assets/BurgerIcon';
+import DropdownMenu from '../DropdownMenu/DropdownMenu';
 import styles from './Header.module.css';
 
-export type BurgerIconprops = {
-  activeButton: boolean;
-  onClick: () => void;
-};
-
-function Header({ activeButton, onClick }: BurgerIconprops): JSX.Element {
-  const active = {
-    fill: 'var(--text-primary)',
-  };
-
-  const inactive = {
-    fill: 'var(--color-secondary)',
-  };
-
+function Header(): JSX.Element {
   return (
     <header className={`${styles.header}`}>
-      <BurgerIcon
-        onClick={onClick}
-        className={styles.burgerIcon}
-        {...(activeButton === true ? active : inactive)}
-      />
+      <DropdownMenu activeButton={false} onClick={() => console.log('Klick')} />
       <h1 className={styles.heading}> ANGEBOTS RECHNER </h1>
     </header>
   );
