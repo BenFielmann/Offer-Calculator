ce093dac214e606f6a967735eb5116341bb34fb2
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-24 13:40:10 +0200

    ✨ Create home page

--- src/app/Pages/Home/Home.tsx
+++ src/app/Pages/Home/Home.tsx
@@ -1,1 +1,28 @@
-export {};
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Navigation from '../../components/Navigation/Navigation';
+import Cards from '../../components/Cards/Cards';
+import styles from './Home.module.css';
+import { useHistory } from 'react-router';
+
+export default function Home(): JSX.Element {
+  const history = useHistory();
+  return (
+    <div className={styles.container}>
+      <Header name="ANGEBOTSRECHNER" />
+      <div className={styles.cardWrapper}>
+        <Cards
+          onClick={() => history.push('/industries')}
+          name="BRANCHEN PROFILE"
+          type="chose"
+        />
+        <Cards
+          onClick={() => history.push('/newobject')}
+          name="NEUES OBJECT"
+          type="chose"
+        />
+      </div>
+      <Navigation callToAction={false} isFirstStep={true} isHomeActive={true} />
+    </div>
+  );
+}
