b3e0e103b1619b310fe6076191213edf07d3acda
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-24 14:53:23 +0200

    🔥 Remove dead code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Home from './Pages/Home/Home';
-/*import ProfileOverview from './Pages/ProfileOverview/ProfileOverview';*/
 
 import styles from './App.module.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -20,6 +19,3 @@ function App(): JSX.Element {
 }
 
 export default App;
-
-/*<img src={logo} className={styles['App-logo']} alt="logo" />
-import logo from './components/assets/Worker.png';*/
