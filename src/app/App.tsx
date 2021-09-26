758c2e6ced80efe59a6b611c06c5480bbf574b30
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-27 00:34:59 +0200

    ✨Create date route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './Pages/Home/Home';
-
+import Date from './Pages/Date/Date';
 import styles from './App.module.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -9,6 +9,9 @@ function App(): JSX.Element {
     <BrowserRouter>
       <div className={styles.App}>
         <Switch>
+          <Route path="/date">
+            <Date />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
