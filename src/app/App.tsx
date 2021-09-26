28ff3d03f934cef142f34b19edb0f56b40b65ca5
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-27 00:06:33 +0200

    ✨Create calculate route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './Pages/Home/Home';
-
+import Calculate from './Pages/Calculate/Calculate';
 import styles from './App.module.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -9,6 +9,9 @@ function App(): JSX.Element {
     <BrowserRouter>
       <div className={styles.App}>
         <Switch>
+          <Route path="/calculate">
+            <Calculate />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
