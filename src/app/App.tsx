72df6f4e83381768dbe092d21a12a4942885f9dc
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-27 02:21:23 +0200

    ✨Create customerdetails route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './Pages/Home/Home';
-
+import CustomerDetails from './Pages/CustomerDetails/CustomerDetails';
 import styles from './App.module.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -9,6 +9,9 @@ function App(): JSX.Element {
     <BrowserRouter>
       <div className={styles.App}>
         <Switch>
+          <Route path="/customerdetails">
+            <CustomerDetails />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
