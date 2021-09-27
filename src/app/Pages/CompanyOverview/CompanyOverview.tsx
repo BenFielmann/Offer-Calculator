480f3e8ff6d99db4540b373b8d065947e0b7bc7c
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-09-27 17:09:50 +0200

    ✨ Create basic companyoverview page

--- src/app/Pages/CompanyOverview/CompanyOverview.tsx
+++ src/app/Pages/CompanyOverview/CompanyOverview.tsx
@@ -1,1 +1,41 @@
-export {};
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Navigation from '../../components/Navigation/Navigation';
+import Textarea from '../../components/Textarea/Textarea';
+import styles from './CompanyOverview.module.css';
+import { useHistory } from 'react-router';
+
+export default function CompanyOverview(): JSX.Element {
+  const history = useHistory();
+  return (
+    <div className={styles.container}>
+      <Header name="FIRMEN ÜBERSICHT" />
+      <main
+        onClick={() => history.push('/profielsettings')}
+        className={styles.cardWrapper}
+      >
+        <Textarea
+          isEditable={false}
+          setIsEditable={console.log}
+          onDeleteClick={console.log}
+          companyValue="Muster Firma"
+          ceoValue="Muster Mann"
+          phoneNumberValue="0...."
+          mobileValue="0...."
+          emailValue="Muster@mail.de"
+          urlValue="www.Muster.de"
+          streetValue="Muster Straße"
+          plzValue="0...."
+          cityValue="Muster Stadt"
+          managerValue="Muster Frau"
+          managerPhoneValue="0...."
+        />
+      </main>
+      <Navigation
+        callToAction={true}
+        isFirstStep={false}
+        isHomeActive={false}
+      />
+    </div>
+  );
+}
