e52e9711d207802c2879fdb4db98e9648beceec1
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-10-01 16:06:38 +0200

    💄 set stroke to camelCase

--- src/app/components/assets/SubmitIcon.tsx
+++ src/app/components/assets/SubmitIcon.tsx
@@ -16,23 +16,23 @@ export default function SubmitIcon(
         d="M14.375 14.375V3.125H26.875V26.875H3.125V14.375H14.375Z"
         fill="#33FF00"
         stroke="#252A2E"
-        stroke-width="2.08333"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.08333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M20.625 8.125V20.625H8.125"
         stroke="black"
-        stroke-width="2.08333"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.08333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.625 18.125L8.125 20.625L10.625 23.125"
         stroke="black"
-        stroke-width="2.08333"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.08333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
