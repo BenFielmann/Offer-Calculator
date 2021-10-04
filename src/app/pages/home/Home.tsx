7e8176d6b577bec3845dfb078cf3e8e06158e4ac
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-10-04 05:23:34 +0200

    🙄Prettier

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Navigation from '../../components/Navigation/Navigation';
