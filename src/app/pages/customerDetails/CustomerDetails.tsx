0365139babcffc6ef7aca1bfa430dbf62735cab3
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-10-04 04:35:54 +0200

    ✨ Create custom hook for customerDetails

--- src/app/pages/customerDetails/CustomerDetails.tsx
+++ src/app/pages/customerDetails/CustomerDetails.tsx
@@ -1,48 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import useCustomerDetails from '../../hooks/useCustomerDetails';
 import Header from '../../components/Header/Header';
 import Navigation from '../../components/Navigation/Navigation';
 import Textarea from '../../components/Textarea/Textarea';
 import styles from './CustomerDetails.module.css';
 
 export default function CustomerDetails(): JSX.Element {
+  const [companyValue, setCompanyValue] = useState('');
+  const [ceoValue, setCeoValue] = useState('');
+  const [phoneNumberValue, setPhoneNumberValue] = useState('');
+  const [mobileValue, setMobileValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [urlValue, setUrlValue] = useState('');
+  const [streetValue, setStreetValue] = useState('');
+  const [plzValue, setPlzValue] = useState('');
+  const [cityValue, setCityValue] = useState('');
+  const [priceValue, setPriceValue] = useState('');
+  const [managerValue, setManagerValue] = useState('');
+  const [managerPhoneValue, setManagerPhoneValue] = useState('');
+
+  const { addCustomerDetails } = useCustomerDetails();
+  const history = useHistory();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    addCustomerDetails({
+      companyValue: companyValue,
+      ceoValue: ceoValue,
+      phoneNumberValue: phoneNumberValue,
+      mobileValue: mobileValue,
+      emailValue: emailValue,
+      urlValue: urlValue,
+      streetValue: streetValue,
+      plzValue: plzValue,
+      cityValue: cityValue,
+      priceValue: priceValue,
+      managerValue: managerValue,
+      managerPhoneValue: managerPhoneValue,
+    });
+    history.push('/newobject');
+  }
+
   return (
     <div className={styles.container}>
-      <Header name="KUNDEN DATEN" />
+      <Header name="KUNDENDATEN" />
       <main className={styles.cardWrapper}>
-        <Textarea
-          isEditable={true}
-          setIsEditable={console.log}
-          onDeleteClick={console.log}
-          companyValue="Muster Firma"
-          setCompanyValue={console.log}
-          ceoValue=""
-          setCeoValue={console.log}
-          phoneNumberValue=""
-          setPhoneNumberValue={console.log}
-          mobileValue=""
-          setMobileValue={console.log}
-          emailValue=""
-          setEmailValue={console.log}
-          urlValue=""
-          setUrlValue={console.log}
-          streetValue=""
-          setStreetValue={console.log}
-          plzValue=""
-          setPlzValue={console.log}
-          cityValue=""
-          setCityValue={console.log}
-          managerValue=""
-          setManagerValue={console.log}
-          managerPhoneValue=""
-          setManagerPhoneValue={console.log}
-        />
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <Textarea
+            isEditable={true}
+            setIsEditable={console.log}
+            onDeleteClick={console.log}
+            companyValue={companyValue}
+            setCompanyValue={setCompanyValue}
+            ceoValue={ceoValue}
+            setCeoValue={setCeoValue}
+            phoneNumberValue={phoneNumberValue}
+            setPhoneNumberValue={setPhoneNumberValue}
+            mobileValue={mobileValue}
+            setMobileValue={setMobileValue}
+            emailValue={emailValue}
+            setEmailValue={setEmailValue}
+            urlValue={urlValue}
+            setUrlValue={setUrlValue}
+            streetValue={streetValue}
+            setStreetValue={setStreetValue}
+            plzValue={plzValue}
+            setPlzValue={setPlzValue}
+            cityValue={cityValue}
+            setCityValue={setCityValue}
+            priceValue={priceValue}
+            setPriceValue={setPriceValue}
+            managerValue={managerValue}
+            setManagerValue={setManagerValue}
+            managerPhoneValue={managerPhoneValue}
+            setManagerPhoneValue={setManagerPhoneValue}
+          />
+        </form>
       </main>
       <Navigation
         callToAction={true}
         isFirstStep={false}
         isHomeActive={false}
         goBack={'/newobject'}
-        goForward={''}
+        goForward={'/'}
       />
     </div>
   );
